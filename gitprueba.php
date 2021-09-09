381a2a949c62eea8be28d57a8d43b5d5ee047353
jmarquez-urq <jmarquez.urquiza@gmail.com>
2021-09-09 12:33:49 -0300

    Revert "Agrega menores de edad"
    This reverts commit 38bfd9d7d36d4d215eebd04a5b1e09a5a493e484.

--- gitprueba.php
+++ gitprueba.php
@@ -4,20 +4,19 @@ $edad = $_GET ['edad'];
 $ciudad = $_GET ['ciudad'];
 $continente = $_GET ['continente'];
 $mayoria_edad = 65;
-$menor_de_edad = 18;
 
 
 if ($edad >= $mayoria_edad){
-    echo "$nombre es adulto mayor";
-} elseif ($edad < $menor_de_edad) {
-    echo " $nombre es menor de edad";
-} else {
-    echo " $nombre es adulto";
-}
+    echo "$nombre  es mayor de edad";
+    } else
+   { echo " $nombre  no es mayor de edad";}
 
-if ($continente != "Latinoamerica del sur") {
-    echo " y no es latino";
-} else {
-    echo ", es latino y de $ciudad";
-}
+    if ($continente != "Latinoamerica del sur")
+        {echo " y no es latino";}
+    else{
+        echo ", es latino y de $ciudad";
+        }
+
+
+
 ?>
